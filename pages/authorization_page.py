b4cb2badfa229ce20736a472087eb514c6c08192
DannyaKenpachi <mezendaniil@gmail.com>
2026-01-06 12:33:08 +0200

    create empty conftest and create authorization_page

---
+++ pages/authorization_page.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import Page
+
+class AuthorizationPage():
+    def __init__(self, page: Page):
+        self.page = page
+        self.username = page.locator("[data-test=\"username\"]")
+        self.password = page.locator("[data-test=\"password\"]")
+        self.button_to_login = page.locator("[data-test=\"login-button\"]")
+    
+    def login(self):
+        self.username.fill('standard_user')
+        self.password.fill('secret_sauce')
+        self.button_to_login.click()
