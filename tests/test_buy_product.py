8c603348ab930dbcf193d4b4b3721035fbbce2ec
DannyaKenpachi <mezendaniil@gmail.com>
2026-01-07 13:27:02 +0200

    create test_buy_product

---
+++ tests/test_buy_product.py
@@ -0,0 +1,18 @@
+import pytest
+import allure
+
+def test_buy_product(authorization_page, main_page, cart_page, checkout_page, confirmation_page, finish_page):
+    authorization_page.login()
+
+    main_page.add_to_cart()
+    main_page.open_cart()
+
+    cart_page.check_product()
+    cart_page.buy_product()
+
+    checkout_page.send_data()
+
+    confirmation_page.confirmation_order()
+
+    finish_page.check_finish()
+    finish_page.back_to_home()
