3b152ab17c0d8ae805c9f50eba2e9f780d11b9fe
DannyaKenpachi <mezendaniil@gmail.com>
2026-01-07 13:35:39 +0200

    add product to functions

--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -4,15 +4,15 @@ import allure
 def test_buy_product(authorization_page, main_page, cart_page, checkout_page, confirmation_page, finish_page):
     authorization_page.login()
 
-    main_page.add_to_cart()
+    main_page.add_to_cart('Sauce Labs Backpack')
     main_page.open_cart()
 
     cart_page.check_product()
-    cart_page.buy_product()
+    cart_page.buy_product('Sauce Labs Backpack')
 
     checkout_page.send_data()
 
-    confirmation_page.confirmation_order()
+    confirmation_page.confirmation_order('Sauce Labs Backpack')
 
     finish_page.check_finish()
     finish_page.back_to_home()
