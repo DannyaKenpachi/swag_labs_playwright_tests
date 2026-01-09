b5e0ccc9b620f4b87a74c73e0195dccd38b2faa5
DannyaKenpachi <mezendaniil@gmail.com>
2026-01-09 21:33:01 +0200

    create gitignore, add allure in test_buy_product

--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -1,18 +1,25 @@
 import pytest
 import allure
 
+@allure.feature('Покупка "Sauce Labs Backpack"')
 def test_buy_product(authorization_page, main_page, cart_page, checkout_page, confirmation_page, finish_page):
-    authorization_page.login()
+    with allure.step('Авторизация'):
+        authorization_page.login()
 
-    main_page.add_to_cart('Sauce Labs Backpack')
-    main_page.open_cart()
+    with allure.step('Покупка товара'):
+        main_page.add_to_cart('Sauce Labs Backpack')
 
-    cart_page.check_product('Sauce Labs Backpack')
-    cart_page.buy_product()
+    with allure.step('Переход в корзину'):    
+        main_page.open_cart()
+        cart_page.check_product('Sauce Labs Backpack')
+    
+    with allure.step('Заполнение данных'):
+        cart_page.buy_product()
+        checkout_page.send_data()
 
-    checkout_page.send_data()
-
-    confirmation_page.confirmation_order('Sauce Labs Backpack')
-
-    finish_page.check_finish()
-    finish_page.back_to_home()
+    with allure.step('Подтверждение покупки'):
+        confirmation_page.confirmation_order('Sauce Labs Backpack')
+        finish_page.check_finish()
+        
+    with allure.step('Возвращение обратно на главную страницу'):    
+        finish_page.back_to_home()
